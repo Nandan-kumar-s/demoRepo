22902c0ff9ab1f6137187bcb228d99c07efb1768
Nandan-kumar-s <108354718+Nandan-kumar-s@users.noreply.github.com>
2025-06-11 12:31:23 +0530

    Update test.java

--- test.java
+++ test.java
@@ -1,5 +1,5 @@
 public class test{
   public static void main(String[] args){
-    System.out.println("Hello from Jenkins java Build!");
+    System.out.println("Hello from  new updated Jenkins java Build!");
   }
 }
